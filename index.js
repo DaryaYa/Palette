71c48c03930454e41fa4d5f436d54f8078eaba44
DaryaYa <lyalyaka@mail.ru>
2019-11-15 12:19:44 +0300

    refactor: apply eslint

--- index.js
+++ index.js
@@ -10,124 +10,118 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let color = 'green';
-let currentColor = document.querySelector('.current-color');
-let prevColor = document.querySelector('.prev-color');
-let pencil = document.querySelector('.pencil');
-let colorPicker = document.querySelector('.colorPicker');
+const currentColor = document.querySelector('.current-color');
+const prevColor = document.querySelector('.prev-color');
+const pencil = document.querySelector('.pencil');
+const colorPicker = document.querySelector('.colorPicker');
 
 
 function draw(e) {
-    if (!isDrawing) return;
+  if (!isDrawing) return;
 
-    ctx.beginPath();
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
-    ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  ctx.strokeStyle = color;
 
-    [lastX, lastY] = [e.offsetX, e.offsetY];
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
 canvas.addEventListener('mousedown', (e) => {
-    isDrawing = true;
-    [lastX, lastY] = [e.offsetX, e.offsetY];
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 });
 
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
-let img = document.getElementById('#img');
+const img = document.getElementById('#img');
 
 function drawPic() {
-    img.onload;
-    ctx.drawImage(img, 0, 0, 512, 512);
+  img.onload;
+  ctx.drawImage(img, 0, 0, 512, 512);
 }
 
 function fourByFour(frame, ctx) {
-    let scale1 = 512 / 4;
-    frame.forEach((raw, i) => {
-        raw.forEach((column, j) => {
-            ctx.fillStyle = '#' + column;
-            ctx.fillRect(j * scale1, i * scale1, (j + 1) * scale1, (i + 1) * scale1);
-        });
+  const scale1 = 512 / 4;
+  frame.forEach((raw, i) => {
+    raw.forEach((column, j) => {
+      ctx.fillStyle = `#${column}`;
+      ctx.fillRect(j * scale1, i * scale1, (j + 1) * scale1, (i + 1) * scale1);
     });
+  });
 }
 
 function draw32x32(frame, ctx) {
-    let scale1 = 512 / 32;
-    frame.forEach((raw, i) => {
-        raw.forEach((column, j) => {
-            ctx.fillStyle = 'rgba(' + column[0] + ',' + column[1] + ',' + column[2] + ')';
-            ctx.fillRect(j * scale1, i * scale1, (j + 1) * scale1, (i + 1) * scale1);
-        });
+  const scale1 = 512 / 32;
+  frame.forEach((raw, i) => {
+    raw.forEach((column, j) => {
+      ctx.fillStyle = `rgba(${column[0] },${column[1] },${column[2] })`;
+      ctx.fillRect(j * scale1, i * scale1, (j + 1) * scale1, (i + 1) * scale1);
     });
+  });
 }
 
-let draw32 = document.querySelector('.draw32x32');
+const draw32 = document.querySelector('.draw32x32');
 
 draw32.onclick = () => fetch('./data/32x32.json')
-    .then(response => response.json().then(json => draw32x32(json, ctx)));
+  .then((response) => response.json().then((json) => draw32x32(json, ctx)));
 
-let draw4x4 = document.querySelector('.draw4x4');
+const draw4x4 = document.querySelector('.draw4x4');
 
 draw4x4.onclick = () => fetch('./data/4x4.json')
-    .then(response => response.json().then(json => fourByFour(json, ctx)));
+  .then((response) => response.json().then((json) => fourByFour(json, ctx)));
 
-let pic = document.querySelector('.pic');
+const pic = document.querySelector('.pic');
 
 pic.addEventListener('click', drawPic);
 
 document.addEventListener('keydown', (event) => {
-    if (event.code === 'KeyP') { setTimeout(() => {
-        pencil.classList.toggle('active');
+  if (event.code === 'KeyP') {
+    setTimeout(() => {
+      pencil.classList.toggle('active');
     }, 1000);
-        
-        currentColor.style.backgroundColor = preview.style.background;
-        color = currentColor.style.backgroundColor;
-        draw();
-    }
+
+    currentColor.style.backgroundColor = preview.style.background;
+    color = currentColor.style.backgroundColor;
+    draw();
+  }
 });
-pencil.addEventListener('click', ()=> {
-        currentColor.style.backgroundColor = preview.style.background;
-        color = currentColor.style.backgroundColor;
-        draw();
+pencil.addEventListener('click', () => {
+  currentColor.style.backgroundColor = preview.style.background;
+  color = currentColor.style.backgroundColor;
+  draw();
 });
 
-let red = document.querySelector('.red');
-let blue = document.querySelector('.blue');
-red.onclick = () => {preview.style.background = currentColor.style.backgroundColor;
-    currentColor.style.backgroundColor = 'red';
-color = 'red'};
-blue.onclick = () => {preview.style.background = currentColor.style.backgroundColor;
-    currentColor.style.backgroundColor = 'blue';
-color = 'blue'};
+const red = document.querySelector('.red');
+const blue = document.querySelector('.blue');
+red.onclick = () => {
+  preview.style.background = currentColor.style.backgroundColor;
+  currentColor.style.backgroundColor = 'red';
+  color = 'red';
+};
+blue.onclick = () => {
+  preview.style.background = currentColor.style.backgroundColor;
+  currentColor.style.backgroundColor = 'blue';
+  color = 'blue';
+};
 
-
-/*
- * Canvas.onclick = function(event) {
- *     let x = event.pageX;
- *     let y = event.pageY;
- *     // Look what color the pixel at the screenshot is
- *     let p = ctx.getImageData(x, y, 1, 1).data;
- *     console.log('rgb(' + p[0] + ',' + p[1] + ',' + p[2] + ')');
- * };
- */
 let preview = document.querySelector('.preview');
 let x = '';
 let y = '';
 
 canvas.addEventListener('mousedown', (e) => {
-    
-    [x, y] = [e.offsetX, e.offsetY];
-    let p = ctx.getImageData(x, y, 1, 1).data;
-    preview.style.background = 'rgb(' + p[0] + ',' + p[1] + ',' + p[2] + ')';
-    console.log('rgb(' + p[0] + ',' + p[1] + ',' + p[2] + ')');
-
+  [x, y] = [e.offsetX, e.offsetY];
+  const p = ctx.getImageData(x, y, 1, 1).data;
+  preview.style.background = `rgb(${p[0]},${p[1]},${p[2]})`;
+  console.log(`rgb(${p[0]},${p[1]},${p[2]})`);
 });
 
+
 function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
-let clear = document.querySelector('.clear');
+const clear = document.querySelector('.clear');
 clear.addEventListener('click', clearCanvas);
