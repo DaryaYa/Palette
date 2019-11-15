3af107d075e8f064ee65d486dbc379a1cd5f055f
DaryaYa <lyalyaka@mail.ru>
2019-11-15 13:44:31 +0300

    feat: apply description

--- index.js
+++ index.js
@@ -14,7 +14,19 @@ const currentColor = document.querySelector('.current-color');
 const prevColor = document.querySelector('.prev-color');
 const pencil = document.querySelector('.pencil');
 const colorPicker = document.querySelector('.colorPicker');
+const img = document.getElementById('#img');
+const draw32 = document.querySelector('.draw32x32');
+const draw4x4 = document.querySelector('.draw4x4');
+const pic = document.querySelector('.pic');
+const red = document.querySelector('.red');
+const blue = document.querySelector('.blue');
+const clear = document.querySelector('.clear');
 
+let range = document.querySelector('.range');
+let colorRange = document.querySelector('.color-range');
+let preview = document.querySelector('.preview');
+let x = '';
+let y = '';
 
 function draw(e) {
   if (!isDrawing) return;
@@ -37,8 +49,6 @@ canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
-const img = document.getElementById('#img');
-
 function drawPic() {
   img.onload;
   ctx.drawImage(img, 0, 0, 512, 512);
@@ -58,45 +68,34 @@ function draw32x32(frame, ctx) {
   const scale1 = 512 / 32;
   frame.forEach((raw, i) => {
     raw.forEach((column, j) => {
-      ctx.fillStyle = `rgba(${column[0] },${column[1] },${column[2] })`;
+      ctx.fillStyle = `rgba(${column[0]},${column[1]},${column[2]})`;
       ctx.fillRect(j * scale1, i * scale1, (j + 1) * scale1, (i + 1) * scale1);
     });
   });
 }
 
-const draw32 = document.querySelector('.draw32x32');
-
 draw32.onclick = () => fetch('./data/32x32.json')
   .then((response) => response.json().then((json) => draw32x32(json, ctx)));
 
-const draw4x4 = document.querySelector('.draw4x4');
-
 draw4x4.onclick = () => fetch('./data/4x4.json')
   .then((response) => response.json().then((json) => fourByFour(json, ctx)));
-
-const pic = document.querySelector('.pic');
 
 pic.addEventListener('click', drawPic);
 
 document.addEventListener('keydown', (event) => {
   if (event.code === 'KeyP') {
-    setTimeout(() => {
-      pencil.classList.toggle('active');
-    }, 1000);
-
     currentColor.style.backgroundColor = preview.style.background;
     color = currentColor.style.backgroundColor;
     draw();
   }
 });
+
 pencil.addEventListener('click', () => {
   currentColor.style.backgroundColor = preview.style.background;
   color = currentColor.style.backgroundColor;
   draw();
 });
 
-const red = document.querySelector('.red');
-const blue = document.querySelector('.blue');
 red.onclick = () => {
   preview.style.background = currentColor.style.backgroundColor;
   currentColor.style.backgroundColor = 'red';
@@ -108,9 +107,15 @@ blue.onclick = () => {
   color = 'blue';
 };
 
-let preview = document.querySelector('.preview');
-let x = '';
-let y = '';
+range.onclick = () => {
+  preview.style.background = currentColor.style.backgroundColor;
+  currentColor.style.backgroundColor = colorRange.value;
+  color = colorRange.value;
+}
+
+prevColor.onclick = () => {
+  color = preview.style.background;
+}
 
 canvas.addEventListener('mousedown', (e) => {
   [x, y] = [e.offsetX, e.offsetY];
@@ -119,9 +124,14 @@ canvas.addEventListener('mousedown', (e) => {
   console.log(`rgb(${p[0]},${p[1]},${p[2]})`);
 });
 
+colorPicker.onclick = (e) => {
+  [x, y] = [e.offsetX, e.offsetY];
+  const p = ctx.getImageData(x, y, 1, 1).data;
+  preview.style.background = `rgb(${p[0]},${p[1]},${p[2]})`;
+};
 
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
-const clear = document.querySelector('.clear');
+
 clear.addEventListener('click', clearCanvas);
